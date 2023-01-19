46f47cd2b0230bab67cded3c8148a00f0c8cc1c4
Filip <filip4125@gmail.com>
2023-01-19 16:28:01 +0100

    Kreirana funkcionalnost za učitavanje nivoa uz pomoć SceneManagera

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.tag == "Ground")
         {
-            Debug.Log("Ode glava");
+            SceneManager.LoadScene(0);
         }
     }
-
 }
