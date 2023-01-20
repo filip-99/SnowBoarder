d1d97ec4767dd180d224ba8f64b0ccfb450544b8
Filip <filip4125@gmail.com>
2023-01-20 11:15:16 +0100

    Urađeno kašnjenje nakon što igrač pređe cilj i pogine

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -5,11 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float delay = 1f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(0);
+            Invoke("NextLevel", delay);
         }
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 }
