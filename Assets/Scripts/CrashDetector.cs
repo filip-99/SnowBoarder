8a80dfc607cd5f9f1279ce685303a8a07e5a78bc
Filip <filip4125@gmail.com>
2023-01-23 16:49:10 +0100

    Dodate su dve Particle System komponente na cilju i kada igrač pogine. Urađena je i njihova funkcionalnost.

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.2f;
+    [SerializeField] ParticleSystem crashEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("NextLevel", delay);
         }
     }
