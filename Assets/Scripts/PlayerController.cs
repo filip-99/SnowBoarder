08e193f1a0a4c6c5a1ad5621048a150400081b60
Filip <filip4125@gmail.com>
2023-01-24 15:05:12 +0100

    Dodata funkcionalnost za kontrolisanje brzine igrača

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,49 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30;
+    [SerializeField] float baseSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+
     Rigidbody2D rb2D;
+    SurfaceEffector2D surfaceEffector2D;
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
+    {
+        RotatePlayer();
+        RespontToBoost();
+    }
+
+    void RespontToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            surfaceEffector2D.speed = slowSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2D.AddTorque(torqueAmount);
         }
 
-        else  if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2D.AddTorque(-torqueAmount);
         }
