95d2291385d6ef05f55585a221b7fb9a86c74bd4
Filip <filip4125@gmail.com>
2023-01-25 11:43:27 +0100

    Ograničena mogućnost da igrač ne može aktivirati više puta zvučni efekat i efekat klizanja

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,15 +8,17 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float loadDelay = 0.2f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Ground")
+        if (other.tag == "Ground" && !hasCrashed)
         {
             crashEffect.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             GetComponent<PlayerController>().DisableControls();
             Invoke("ReloadScene", loadDelay);
+            hasCrashed = true;
         }
     }
 
